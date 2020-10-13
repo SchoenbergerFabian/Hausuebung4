c0f5d225971648020262430a44d8d3f16d7075b8
Infendro <fabianschoenberger2301@gmail.com>
2020-10-13 19:31:59 +0200

    Beispiel3: added method "main" to class "JavaStreamsTester"

--- Beispiel3/src/JavaStreamsTester.java
+++ Beispiel3/src/JavaStreamsTester.java
@@ -1,7 +1,41 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class JavaStreamsTester {
+
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        strings.add("A");
+        strings.add("BB");
+        strings.add("");
+        strings.add("");
+        strings.add("CCC");
+        strings.add("");
+        strings.add("DDD");
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(22);
+        numbers.add(333);
+        numbers.add(44);
+        numbers.add(5);
+
+        print("getCountEmptyString", getCountEmptyString(strings));
+        print("getCountLength3", getCountLength3(strings));
+        print("deleteEmptyStrings", deleteEmptyStrings(strings));
+        print("getMergedString", getMergedString(strings,";"));
+        print("getSquares", getSquares(numbers));
+        print("getMax", getMax(numbers));
+        print("getMin", getMin(numbers));
+        print("getSum", getSum(numbers));
+        print("getAverage", getAverage(numbers));
+    }
+
+    private static void print(String info, Object result){
+        System.out.println(info+"\n" +
+                result + "\n");
+    }
 
     private static int getCountEmptyString(List<String> strings){
         return strings.stream()
@@ -17,13 +51,13 @@ public class JavaStreamsTester {
 
     private static List<String> deleteEmptyStrings(List<String> strings){
         return strings.stream()
-                .filter(string -> string.length()==0)
+                .filter(string -> string.length()!=0)
                 .collect(Collectors.toList());
     }
 
     private static String getMergedString(List<String> strings, String separator){
         return strings.stream()
-                .reduce((string1,string2) -> string1+string2)
+                .reduce((string1,string2) -> string1+separator+string2)
                 .orElse(null);
     }
 
